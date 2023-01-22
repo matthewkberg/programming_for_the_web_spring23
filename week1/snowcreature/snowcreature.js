d45717474791450b78e6dd4128cf4ae3c03eb9d1
matthewkberg <103538467+matthewkberg@users.noreply.github.com>
2023-01-22 15:27:18 -0800

    Added background grid

--- week1/snowcreature/snowcreature.js
+++ week1/snowcreature/snowcreature.js
@@ -2,4 +2,8 @@ let grid = undefined;
 function setup() {
     createCanvas(1000,800);
     background("#9c92b0")
+    grid = loadImage("imgs/100px_grid.png")
+}
+function draw() {
+    background(grid);
 }
