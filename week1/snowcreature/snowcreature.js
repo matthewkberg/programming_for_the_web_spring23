4e7befdbe947112d6e21953932d3b1ae338bcbb6
matthewkberg <103538467+matthewkberg@users.noreply.github.com>
2023-01-22 16:07:44 -0800

    Created hat, eyes and mouth

--- week1/snowcreature/snowcreature.js
+++ week1/snowcreature/snowcreature.js
@@ -15,4 +15,27 @@ function draw() {
     ellipse(500, 425, 200);
     //snowcreature head
     ellipse(500, 275, 150);
+    // hat brim
+    stroke("#e0d09d");
+    strokeWeight(30);
+    line(400, 225, 600, 225);
+    // hat body
+    fill("#e0d09d");
+    quad(415, 150, 595, 150, 575, 200, 435, 200);
+    // eyes
+    stroke(0);
+    strokeWeight(40);
+    point(475, 275);
+    point(525, 275);
+    // pupils
+    stroke("#e8e8e8");
+    strokeWeight(15);
+    point(465, 275);
+    point(515, 275);
+    // mouth
+    noFill();
+    stroke(0);
+    strokeWeight(5);
+    arc(500, 310, 50, 50, 0, 2.63, HALF_PI);
+
 }
