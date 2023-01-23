661f9021a6b31666e039aaa698b309a56ba47136
matthewkberg <103538467+matthewkberg@users.noreply.github.com>
2023-01-22 16:21:49 -0800

    added color prompts and grass

--- week1/snowcreature/snowcreature.js
+++ week1/snowcreature/snowcreature.js
@@ -1,4 +1,6 @@
 let grid = undefined;
+let stroke1 = prompt("enter a basic color name in lowercase", "yellow")
+let stroke2 = prompt("enter another basic color lowercase", "magenta")
 function setup() {
     createCanvas(1000,800);
     background("#9c92b0")
@@ -6,6 +8,10 @@ function setup() {
 }
 function draw() {
     background(grid);
+    // grass
+    fill("#9caf88");
+    stroke("#718e8b");
+    ellipse(500, 800, 1150, 400);
     // snowcreature base
     fill("#e8e8e8");
     strokeWeight(10);
@@ -20,7 +26,7 @@ function draw() {
     strokeWeight(30);
     line(400, 225, 600, 225);
     // hat body
-    fill("#b68873");
+    fill(stroke1);
     quad(425, 125, 575, 150, 545, 200, 450, 200);
     // eyes
     stroke(0);
@@ -34,7 +40,7 @@ function draw() {
     point(515, 275);
     // mouth
     noFill();
-    stroke(0);
+    stroke(stroke2);
     strokeWeight(5);
     arc(500, 310, 50, 50, 0, 2.63, HALF_PI);
 
