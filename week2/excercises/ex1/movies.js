707f6c51c1c200bc1995186c9eaa20e1b3a2c96a
matthewkberg <103538467+matthewkberg@users.noreply.github.com>
2023-02-12 16:17:42 -0600

    Adding users fav to my string of favs and create log

--- week2/excercises/ex1/movies.js
+++ week2/excercises/ex1/movies.js
@@ -5,3 +5,7 @@ console.log("My top 5 films", myFavs);
 // Prompt users to enter their favorite film
 const yourFav = window.prompt("Please share your favorite film:");
 console.log("Your favorite film is", yourFav)
+
+// Adding users favorite film to my string of favorite films
+myFavs.push(" & " + yourFav);
+console.log("Our favorite films are", myFavs)
