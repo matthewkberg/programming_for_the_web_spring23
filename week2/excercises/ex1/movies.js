c27da5dc5c09eed3893893654d151305dca636e6
matthewkberg <103538467+matthewkberg@users.noreply.github.com>
2023-02-12 16:13:55 -0600

    Create window prompt and log for users favorite film

--- week2/excercises/ex1/movies.js
+++ week2/excercises/ex1/movies.js
@@ -1,3 +1,7 @@
 // My favorite films
 const myFavs = [ 'Weekend', 'Melancholia', 'The Wizard of Oz', 'Poltergeist', 'Dan in Real Life'];
 console.log("My top 5 films", myFavs);
+
+// Prompt users to enter their favorite film
+const yourFav = window.prompt("Please share your favorite film:");
+console.log("Your favorite film is", yourFav)
