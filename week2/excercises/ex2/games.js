f23b0bc91ac8da1fa262daf66210098e28f39563
matthewkberg <103538467+matthewkberg@users.noreply.github.com>
2023-02-12 16:42:23 -0600

    Create object for myFavGameTwo / Dream Daddy details

--- week2/excercises/ex2/games.js
+++ week2/excercises/ex2/games.js
@@ -5,5 +5,14 @@ const myFavGameOne = {
     numberOfPlayers: 'Single Player', 
     rating: 'E for Everyone', 
     shortDescription: 'Stardew Valley is a simulation role-playing game where players take the role of a character who inherits a dilapidated farm in a place known as Stardew Valley.'
-}
-console.log("Stardew Valley details", myFavGameOne)
+};
+console.log("Stardew Valley details", myFavGameOne);
+
+const myFavGameTwo = { 
+    title: 'Dream Daddy', 
+    type: 'Dating Simulator', 
+    numberOfPlayers: 'Single Player', 
+    rating: 'T for Teen', 
+    shortDescription: 'Dream Daddy is an adventure lifestlye simulation game where you play as a Dad and your goal is to meet and romance other hot Dads.'
+};
+console.log("Dream Daddy details", myFavGameTwo);
