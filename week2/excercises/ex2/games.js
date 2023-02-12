7a205fa7ae5b1dd4b39c2794d4a622efafc898e6
matthewkberg <103538467+matthewkberg@users.noreply.github.com>
2023-02-12 16:51:21 -0600

    Create object for myFavGameThree / Super Mario Odyssey details

--- week2/excercises/ex2/games.js
+++ week2/excercises/ex2/games.js
@@ -16,3 +16,12 @@ const myFavGameTwo = {
     shortDescription: 'Dream Daddy is an adventure lifestlye simulation game where you play as a Dad and your goal is to meet and romance other hot Dads.'
 };
 console.log("Dream Daddy details", myFavGameTwo);
+
+const myFavGameThree = { 
+    title: 'Super Mario Odyssey', 
+    type: 'Actio Platformer', 
+    numberOfPlayers: '1 to 2 players', 
+    rating: 'E for Everyone', 
+    shortDescription: 'Join Mario and his new ally Cappy on a massive, globe-trotting 3D adventure to stop Bowser from forcing Princess Peach to marry him.'
+}
+console.log("Super Mario Odyssey details", myFavGameThree);
