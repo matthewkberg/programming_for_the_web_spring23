bdcf9bd5d81f52c43bb7de1380a7f26b7dc6d002
matthewkberg <103538467+matthewkberg@users.noreply.github.com>
2023-02-12 16:35:32 -0600

    Create object for myFavGameOne / Stardew Valley details

---
+++ week2/excercises/ex2/games.js
@@ -0,0 +1,9 @@
+// My three favorite games
+const myFavGameOne = { 
+    title: 'Stardew Valley', 
+    type: 'Indie RPG', 
+    numberOfPlayers: 'Single Player', 
+    rating: 'E for Everyone', 
+    shortDescription: 'Stardew Valley is a simulation role-playing game where players take the role of a character who inherits a dilapidated farm in a place known as Stardew Valley.'
+}
+console.log("Stardew Valley details", myFavGameOne)
