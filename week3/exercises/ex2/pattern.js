ccf4e6b9fb556dbfcbdf49de9686c05c0609d718
Matthew Berg <matthewkberg@gmail.com>
2023-02-13 15:26:59 -0600

    Added parameters to createTile, put hex colors back on first tile

--- week3/exercises/ex2/pattern.js
+++ week3/exercises/ex2/pattern.js
@@ -1,28 +1,31 @@
 function setup() {
     createCanvas(600, 600)
 }
-function createTile(originX, originY) {
+function createTile(originX, originY, tileColor, strokeColor1, strokeColor2, triangleColor, dotColor) {
     translate(originX, originY);
-    fill('#1b3858');
+    fill(tileColor);
     rect(0, 0, 200, 200);
+    // Circles
     noFill();
-    stroke('#d9dbec');
+    stroke(strokeColor1);
     strokeWeight(5);
     circle(100, 100, 20);
     circle(100, 100, 100);
     circle(100, 100, 200);
-    stroke('#1d1b58');
+    stroke(strokeColor2);
     strokeWeight(2);
     circle(100, 100, 50);
     circle(100, 100, 150);
     circle(100, 100, 175);
+    // Triangles
     noStroke();
-    fill('#581b38');
+    fill(triangleColor);
     triangle(30, 75, 58, 20, 86, 75);
     triangle(170, 125, 142, 180, 114, 125);
     triangle(114, 75, 114, 20, 170, 47);
     triangle(30, 152, 86, 125, 86, 180);
-    fill('#5f738a');
+    // Dots on triangles
+    fill(dotColor);
     circle(58, 60, 20);
     circle(142, 140, 20);
     circle(130, 48, 20);
@@ -30,6 +33,10 @@ function createTile(originX, originY) {
 
 }
 function draw() {
-    createTile(0, 0);
+    createTile(0, 0, '#1b3858', '#d9dbec', '#1d1b58', '#581b38', '#5f738a');
     createTile(0, 200);
+    createTile(0, 200);
+    createTile(200, -400);
+    createTile(0, 200);
+    createTile()
 }
