c36887a348b9255014cfb0142c434ad7b21639f3
Matthew Berg <matthewkberg@gmail.com>
2023-02-13 17:11:48 -0600

    assigned a variable to the empty div,  started creating a new element to put an h1 inside of the div

---
+++ week4/ex1/ex1.js
@@ -0,0 +1,7 @@
+// Select empty div and assign to a variable
+const myEmptyDiv = document.querySelector("#myEmptyDiv");
+
+// Put an h1 inside of it
+// Create a new element
+const heading = document.createElement('h1');
+heading.innerHTML = 'Hello world! This is a manipulation';
