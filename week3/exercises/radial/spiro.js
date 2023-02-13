7c5d16ff915b3aad332c5df40ee56d754b52aa13
Matthew Berg <matthewkberg@gmail.com>
2023-02-13 16:22:44 -0600

    Created circle

--- week3/exercises/radial/spiro.js
+++ week3/exercises/radial/spiro.js
@@ -3,3 +3,13 @@ function setup() {
     background(0);
     angleMode(DEGREES)
 }
+
+function draw() {
+    translate(300, 300);
+    rotate(8); // rotates canvas
+    noFill();
+    stroke('white');
+    strokeWeight(2);
+    ellipse(150, 150, 150);
+
+}
