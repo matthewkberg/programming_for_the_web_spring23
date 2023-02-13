d5b87f9f277075c9bcd188c80eb695ab455af5c9
Matthew Berg <matthewkberg@gmail.com>
2023-02-13 17:20:48 -0600

    changed the background color and add pointer style to the heading

--- week4/ex1/ex1.js
+++ week4/ex1/ex1.js
@@ -4,10 +4,11 @@ const myEmptyDiv = document.querySelector("#myEmptyDiv");
 // Put an h1 inside of it
 // Create a new element
 const heading = document.createElement('h1');
-heading.innerHTML = 'Hello world! This is a manipulation';
+heading.innerHTML = 'Hello world! This is a manipulation.';
+heading.style.cursor = 'pointer';
 heading.addEventListener('click', handleHeadingClick);
 myEmptyDiv.appendChild(heading);
 function handleHeadingClick(){
     console.log('Heading has been clicked');
-    document.body.style.backgroundColor = 'aliceblue';
+    document.body.style.backgroundColor = 'magenta';
 }
