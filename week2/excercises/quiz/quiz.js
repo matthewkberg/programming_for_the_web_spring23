bd7362849298beadd060a62843652956d48afcdf
Matthew Berg <matthewkberg@gmail.com>
2023-02-12 21:39:59 -0600

    Created the rest of the questions and answers

--- week2/excercises/quiz/quiz.js
+++ week2/excercises/quiz/quiz.js
@@ -1,6 +1,8 @@
 // My quiz questions and answers
 const quizArray = [
     {question: "Who was the first queen to ever be eliminated from Rupaul's Drag Race?" , answer: "Porkchop."},
-    
+    {question: "Who was the first queen to never have to lip sync for their life on their season of Rupaul's Drag Race?" , answer: "Nina Flowers."},
+    {question: "Which winner of Rupaul's Drag Race was recently named the Queen of All Queens?" , answer: "Jinx Monsoon."},
+    {question: "Who is the only queen to be eliminated from RuPaul's Drag Race for breaking the rules?" , answer: "Willam"}
 
 ]
