df37c99a0c513b227a1291c0348ca4f71eddeffa
Matthew Berg <matthewkberg@gmail.com>
2023-02-13 16:27:22 -0600

    created makeArm and moved function draw inside of it

--- week3/exercises/radial/spiro.js
+++ week3/exercises/radial/spiro.js
@@ -4,14 +4,17 @@ function setup() {
     background(0);
     angleMode(DEGREES)
 }
-
-function draw() {
-    translate(300, 300);
-    rotate(rotateBy); // rotates canvas
+function makeArm() {
     noFill();
     stroke('white');
     strokeWeight(2);
     ellipse(150, 150, 150);
+}
+
+function draw() {
+    translate(300, 300);
+    rotate(rotateBy); // rotates canvas
+    makeArm();
     rotateBy += 8;
 
 }
