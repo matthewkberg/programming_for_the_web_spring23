99b3b78eb9c9c3349837ba51a522a641627a5e55
Matthew Berg <matthewkberg@gmail.com>
2023-02-13 14:58:04 -0600

    Adjusted colors of all shapes

--- week3/exercises/ex1/tile.js
+++ week3/exercises/ex1/tile.js
@@ -4,27 +4,27 @@ function setup() {
 }
 function createTile() {
     translate(0, 0);
-    fill('blue');
+    fill('#1b3858');
     rect(0, 0, 200, 200);
     noFill();
-    stroke('green');
+    stroke('#d9dbec');
     strokeWeight(5);
     circle(100, 100, 20);
     circle(100, 100, 100);
     circle(100, 100, 200);
     noStroke();
-    fill('yellow');
+    fill('#581b38');
     triangle(30, 75, 58, 20, 86, 75);
     triangle(170, 125, 142, 180, 114, 125);
     triangle(114, 75, 114, 20, 170, 47);
     triangle(30, 152, 86, 125, 86, 180);
-    fill('pink');
+    fill('#5f738a');
     circle(58, 60, 20);
     circle(142, 140, 20);
     circle(130, 48, 20);
     circle(70, 153, 20);
     noFill();
-    stroke('orange');
+    stroke('#1d1b58');
     strokeWeight(2);
     circle(100, 100, 50);
     circle(100, 100, 150);
