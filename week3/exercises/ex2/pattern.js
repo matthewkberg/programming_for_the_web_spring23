8a211f99c9f6e866d8f1445b4536ee96d1ca7452
Matthew Berg <matthewkberg@gmail.com>
2023-02-13 16:08:30 -0600

    Adjusted colors of tiles, added strokeWeight1 and strokeWeight2 parameters, adjust stroke sizes

--- week3/exercises/ex2/pattern.js
+++ week3/exercises/ex2/pattern.js
@@ -1,7 +1,7 @@
 function setup() {
     createCanvas(600, 600)
 }
-function createTile(originX, originY, tileColor, strokeColor1, strokeColor2, triangleColor, dotColor) {
+function createTile(originX, originY, tileColor, strokeWeight1, strokeColor1, strokeWeight2, strokeColor2, triangleColor, dotColor) {
     translate(originX, originY);
     fill(tileColor);
     noStroke();
@@ -9,12 +9,12 @@ function createTile(originX, originY, tileColor, strokeColor1, strokeColor2, tri
     // Circles
     noFill();
     stroke(strokeColor1);
-    strokeWeight(5);
+    strokeWeight(strokeWeight1);
     circle(100, 100, 20);
     circle(100, 100, 100);
     circle(100, 100, 200);
     stroke(strokeColor2);
-    strokeWeight(2);
+    strokeWeight(strokeWeight2);
     circle(100, 100, 50);
     circle(100, 100, 150);
     circle(100, 100, 175);
@@ -34,13 +34,13 @@ function createTile(originX, originY, tileColor, strokeColor1, strokeColor2, tri
 
 }
 function draw() {
-    createTile(0, 0, '#1b3858', '#d9dbec', '#1d1b58', '#581b38', '#5f738a');
-    createTile(0, 200, '#1b3858', '#d9dbec', '#1d1b58', '#581b38', '#5f738a');
-    createTile(0, 200, '#1b3858', '#d9dbec', '#1d1b58', '#581b38', '#5f738a');
-    createTile(200, -400, '#1b3858', '#d9dbec', '#1d1b58', '#581b38', '#5f738a');
-    createTile(0, 200, '#1b3858', '#d9dbec', '#1d1b58', '#581b38', '#5f738a');
-    createTile(0, 200, '#1b3858', '#d9dbec', '#1d1b58', '#581b38', '#5f738a');
-    createTile(200, -400, '#1b3858', '#d9dbec', '#1d1b58', '#581b38', '#5f738a');
-    createTile(0, 200, '#1b3858', '#d9dbec', '#1d1b58', '#581b38', '#5f738a');
-    createTile(0, 200, '#1b3858', '#d9dbec', '#1d1b58', '#581b38', '#5f738a');
+    createTile(0, 0, '#1b3858', 2, '#d9dbec', 2, '#1d1b58', '#581b38', '#5f738a');
+    createTile(0, 200, '#a695d3', 4, '#8bc2f5', 7, '#f7c7c7', '#d32626', '#e74438');
+    createTile(0, 200, '#662c92', 6, '#a283be', 1, '#5f5367', '#6888a7', '#d1dbe4');
+    createTile(200, -400, '#031b31', 2, '#351c75', 2, '#6b1b74', '#9e3865', '#ff74ee');
+    createTile(0, 200, '#ce7e00', 4, '#b71368', 7, '#44808d', '#074470', '#d6dc58');
+    createTile(0, 200, '#035393', 6, '#ffdc77', 1, '#f6b26b', '#38b0c2', '#ffffff');
+    createTile(200, -400, '#c5d0b6', 2, '#a0b0bf', 2, '#dcaa91', '#f4f6f1', '#ecdeb1');
+    createTile(0, 200, '#000000', 4, '#696b4d', 7, '#f1c232', '#e1e1db', '#f0f0ed');
+    createTile(0, 200, '#700d14', 6, '#2b3a67', 1, '#41337a', '#06050c', '#a3121c');
 }
