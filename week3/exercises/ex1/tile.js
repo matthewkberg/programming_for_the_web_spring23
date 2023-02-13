e69c70c470e89960209e7c1f6af74dc3a282f739
Matthew Berg <matthewkberg@gmail.com>
2023-02-13 14:08:18 -0600

    Created canvas and tile, added function draw

---
+++ week3/exercises/ex1/tile.js
@@ -0,0 +1,14 @@
+// Create canvas
+function setup() {
+    createCanvas(200, 200);
+}
+function createTile() {
+    translate(0, 0);
+    fill('blue');
+    rect(0, 0, 200, 200);
+
+}
+
+function draw() {
+    createTile();
+}
