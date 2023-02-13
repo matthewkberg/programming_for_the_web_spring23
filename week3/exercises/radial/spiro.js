1b2d010902731eb41a32e58eea3ef2052feb23e7
Matthew Berg <matthewkberg@gmail.com>
2023-02-13 16:54:43 -0600

    added two beziers and played with the colors of each, adjusted the colors of the other shapes

--- week3/exercises/radial/spiro.js
+++ week3/exercises/radial/spiro.js
@@ -8,15 +8,20 @@ function makeArm(rotateBy) {
     let alt = Math.round(rotateBy / 360); // adjust the rotation
     console.log(alt);
     noFill();
-    stroke('white');
+    stroke('lightblue');
     strokeWeight(1);
     ellipse(150, 150 + alt, 150 - alt);
     stroke('magenta');
     strokeWeight(3);
     triangle(alt + 30, alt * 75, 58, alt - 20, 86, 75);
-    stroke('aqua');
+    stroke('yellow');
     strokeWeight(1);
     square(30, 20, 20 * alt);
+    stroke('blue');
+    strokeWeight(4);
+    bezier(alt + 10, 20, 10, alt - 10, 90, 90, 15, 80);
+    bezier(10, alt * 20, alt + 10, 10, 90, 90, 15, 80);
+
 
 }
 
