66a8a614c9329e25df53134434432f264ae71744
Matthew Berg <matthewkberg@gmail.com>
2023-02-13 14:29:08 -0600

    took away the stroke and added a second triangle

--- week3/exercises/ex1/tile.js
+++ week3/exercises/ex1/tile.js
@@ -6,8 +6,10 @@ function createTile() {
     translate(0, 0);
     fill('blue');
     rect(0, 0, 200, 200);
-    fill('yellow')
+    noStroke();
+    fill('yellow');
     triangle(30, 75, 58, 20, 86, 75);
+    triangle(170, 125, 142, 180, 114, 125);
 
 }
 
