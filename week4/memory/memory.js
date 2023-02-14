ecaeb617bbf3ca0df91d97c9109bfa83ca14af5d
Matthew Berg <matthewkberg@gmail.com>
2023-02-14 13:47:32 -0600

    created unique id incrementation

--- week4/memory/memory.js
+++ week4/memory/memory.js
@@ -1,6 +1,7 @@
 let startingX = 100;
 let startingY = 100;
 let myRects =[];
+let startingId = 0;
 // Canvas
 function setup() {
     createCanvas(1200, 1000);
@@ -8,8 +9,9 @@ function setup() {
     for (let k = 0; k < 4; k++) {
             for (let i = 0; i < 5; i++) {
             rect(startingX, startingY, 100, 150, 20);
-            myRects.push({ x: startingX, y: startingY, id: i });
+            myRects.push({ x: startingX, y: startingY, id: startingId });
             startingX += 150;
+            startingId++
         }
         startingY += 200;
         startingX = 100;
