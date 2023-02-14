9cc2b48438eb55edab86aaa311ea2968a395dc95
Matthew Berg <matthewkberg@gmail.com>
2023-02-14 13:29:15 -0600

    created tracking and log for myRects

--- week4/memory/memory.js
+++ week4/memory/memory.js
@@ -1,12 +1,14 @@
-let startingX = 100
-let startingY = 100
-
-
+let startingX = 100;
+let startingY = 100;
+let myRects =[];
+// Canvas
 function setup() {
     createCanvas(1000, 1000);
     background('#361d2e');
     for (let i = 0; i < 5; i++) {
         rect(startingX, startingY, 100, 150, 20);
+        myRects.push({ x: startingX, y: startingY });
         startingX += 150;
     }
+    console.log(myRects);
 }
