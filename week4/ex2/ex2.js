94bbfbeb60b07d9e7635b529502a64e24445cbcb
Matthew Berg <matthewkberg@gmail.com>
2023-02-13 18:06:14 -0600

    added speed variable, set speed to be selected at random between 1 and 3

--- week4/ex2/ex2.js
+++ week4/ex2/ex2.js
@@ -1,17 +1,19 @@
 let rectX = 0;
 let rectY;
+let clickCount = 0;
 const rectWidth = 75;
 const rectHeight = 75;
-let clickCount = 0;
+let speed;
 function setup() {
     createCanvas(500, 500);
-    rectY = random(height - rectHeight); 
+    rectY = random(height - rectHeight); // selects a random placement on the y axis between 75 and 500
+    speed = random(1, 3);
 }
 
 function draw() {
     background(155);
     drawShape();
-    rectX++; // moves box across the canvas on the x axis
+    rectX += speed; // moves box across the canvas on the x axis
     if (rectX > width) {
         noLoop();
         text('Your score was ' + clickCount, 100, 300);
