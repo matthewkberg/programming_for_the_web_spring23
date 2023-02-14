93f1a988f83228ddaebfb7452af0b6ec08f205fd
Matthew Berg <matthewkberg@gmail.com>
2023-02-14 13:43:20 -0600

    created loop for additional rows

--- week4/memory/memory.js
+++ week4/memory/memory.js
@@ -5,10 +5,15 @@ let myRects =[];
 function setup() {
     createCanvas(1000, 1000);
     background('#361d2e');
-    for (let i = 0; i < 5; i++) {
-        rect(startingX, startingY, 100, 150, 20);
-        myRects.push({ x: startingX, y: startingY, id: i });
-        startingX += 150;
+    for (let k = 0; k < 4; k++) {
+            for (let i = 0; i < 5; i++) {
+            rect(startingX, startingY, 100, 150, 20);
+            myRects.push({ x: startingX, y: startingY, id: i });
+            startingX += 150;
+        }
+        startingY += 200;
+        startingX = 100;
+    
     }
     console.log(myRects);
 }
