45498130e8e1ab3b43bbe73fd5304f83692bb29b
Matthew Berg <matthewkberg@gmail.com>
2023-02-16 13:55:01 -0600

    commented out functions that displayed and moved the bubble previously, added bubble variable with new Bubble to setup function, created class Bubble and assigned initial this and that.

--- week5/ex2/class.js
+++ week5/ex2/class.js
@@ -1,27 +1,34 @@
-var bubble = {
-    x: 300,
-    y: 200,
-}
+let bubble;
+
 
 function setup() {
     createCanvas(600, 400);
+    bubble = new Bubble();
+    print(bubble.x, bubble.y);
 }
 
 function draw() {
     background(0);
-    move();
-    display();
+    // move();
+    // display();
 }
 
-function display() {
-    stroke(255);
-    strokeWeight(1);
-    noFill();
-    ellipse(bubble.x, bubble.y, 24, 24);
+class Bubble {
+    constructor() {
+        this.x = 200;
+        this.y = 150;
+    }
 }
 
-function move() {
-    bubble.x = bubble.x + random(-5, 5);
-    bubble.y = bubble.y + random(-5, 5);
+// function display() {
+//     stroke(255);
+//     strokeWeight(1);
+//     noFill();
+//     ellipse(bubble.x, bubble.y, 24, 24);
+// }
+
+// function move() {
+//     bubble.x = bubble.x + random(-5, 5);
+//     bubble.y = bubble.y + random(-5, 5);
     
-}
+// }
