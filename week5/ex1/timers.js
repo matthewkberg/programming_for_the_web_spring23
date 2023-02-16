88e7f7da3ba3d63be07e91295954620ca1635b27
Matthew Berg <matthewkberg@gmail.com>
2023-02-16 12:00:32 -0600

    set up function to draw square

--- week5/ex1/timers.js
+++ week5/ex1/timers.js
@@ -2,4 +2,11 @@
 function setup() {
     createCanvas(500, 500);
     background(0);
+    drawBlock();
+}
+
+// Setup fucntion to draw square
+function drawBlock() {
+    fill(255);
+    rect(0, 0, 50, 50);
 }
