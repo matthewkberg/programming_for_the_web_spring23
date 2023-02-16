9a9d3c22ddf969dbce9cd8b10413148a65cd6a2a
Matthew Berg <matthewkberg@gmail.com>
2023-02-16 12:04:53 -0600

    created variables for drawBlock parameters and passed them in to the function setup

--- week5/ex1/timers.js
+++ week5/ex1/timers.js
@@ -1,12 +1,15 @@
+// Variables for parameters
+let blockX = 0;
+let blockY = 0;
 // Canvas and background
 function setup() {
     createCanvas(500, 500);
     background(0);
-    drawBlock();
+    drawBlock(blockX, blockY, 255);
 }
 
 // Set up function to draw square
 function drawBlock(x, y, color) {
     fill(color || 255);
-    rect(0, 0, 50, 50);
+    rect(x, y, 50, 50);
 }
