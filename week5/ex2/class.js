7d2baf6c4475293262f6ecc62654b073166d0568
Matthew Berg <matthewkberg@gmail.com>
2023-02-16 14:02:06 -0600

    set move and display using this.x and this.y variables

--- week5/ex2/class.js
+++ week5/ex2/class.js
@@ -18,6 +18,19 @@ class Bubble {
         this.x = 200;
         this.y = 150;
     }
+    
+    move() {
+        this.x = this.x + random(-5, 5);
+        this.y = this.y + random(-5, 5);
+    }
+
+    display() {
+        stroke(255);
+        strokeWeight(1);
+        noFill();
+        ellipse(this.x, this.y, 24, 24);
+    }
+
 }
 
 // function display() {
