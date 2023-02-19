0742f509376b2432e77164a7c74bda3ad472be3b
Matthew Berg <matthewkberg@gmail.com>
2023-02-19 13:06:21 -0600

    created card flip action

--- week5/memory/memory.js
+++ week5/memory/memory.js
@@ -25,16 +25,32 @@ class Card {
     }
     // Method = like functions but specific to this class
     show () {
-        fill('#86a397')
-        rect(this.x, this.y, this.width, this.height, 10);
+        if (this.face === DOWN) {
+            fill('#86a397')
+            rect(this.x, this.y, this.width, this.height, 10);
+        } else {
+            fill('#e1b07e')
+            rect(this.x, this.y, this.width, this.height, 10);
+        }
+        
     }
 
     didHit (mouseX, mouseY) {
         if (mouseX >= this.x && mouseX <= this.x + this.width &&
             mouseY >= this.y && mouseY <= this.y + this.height) {
+                this.flip();
                 return true;
         } else {
             return false;
         }
+    }
+
+    flip () {
+        if (this.face === DOWN) {
+            this.face = UP;
+        } else {
+            this.face = DOWN;
+        }
+        this.show();
     }
 }
