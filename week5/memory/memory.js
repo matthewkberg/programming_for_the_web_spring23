8a42045ba34c940fa117c1f0efb33753bcd80799
Matthew Berg <matthewkberg@gmail.com>
2023-02-19 12:58:58 -0600

    Set card hit detection and created constants for the down/back of card, and up/front of card display states

--- week5/memory/memory.js
+++ week5/memory/memory.js
@@ -1,7 +1,15 @@
+let myCard;
+const DOWN = 'down';
+const UP = 'up';
 function setup() {
     createCanvas(1000, 800);
     background('#361d2e');
-    let myCard = new Card();
+    myCard = new Card();
+}
+
+function mousePressed() {
+    console.log(myCard.didHit(mouseX, mouseY));
+    
 }
 
 // Create class for Card
@@ -12,11 +20,21 @@ class Card {
         this.y = 100;
         this.width = 100;
         this.height = 150;
+        this.face = DOWN;
         this.show();
     }
     // Method = like functions but specific to this class
     show () {
         fill('#86a397')
         rect(this.x, this.y, this.width, this.height, 10);
+    }
+
+    didHit (mouseX, mouseY) {
+        if (mouseX >= this.x && mouseX <= this.x + this.width &&
+            mouseY >= this.y && mouseY <= this.y + this.height) {
+                return true;
+        } else {
+            return false;
+        }
     }
 }
