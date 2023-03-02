8496e726ac927aaf6d92bed096cf2634773c22b7
Matthew Berg <matthewkberg@gmail.com>
2023-03-02 13:39:25 -0600

    Added style binding to facts lists

--- week7/ex1/llamas-alpacas.js
+++ week7/ex1/llamas-alpacas.js
@@ -3,6 +3,7 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            // Wiki links
             llamaWiki: "https://en.wikipedia.org/wiki/Llama",
             alpacaWiki: "https://en.wikipedia.org/wiki/Alpaca",
 
