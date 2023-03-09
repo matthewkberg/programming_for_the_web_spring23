acc02a4976627f3c72f7eb5272798fdee5272e4b
Matthew Berg <matthewkberg@gmail.com>
2023-03-09 12:38:38 -0600

    Added initial calculator data for myApp in js

--- Week8/ex2/calc.js
+++ Week8/ex2/calc.js
@@ -3,7 +3,10 @@ const { createApp } = Vue
 createApp ({
     data() {
         return {
-            
+            firstNum: null,
+            secondNum: null,
+            operation: 'add',
+            total: null
         }
     }
 }).mount('#myApp')
