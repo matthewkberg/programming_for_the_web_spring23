6dad983aa20eea06b242ede070247a73e65b125a
Matthew Berg <matthewkberg@gmail.com>
2023-03-09 13:06:34 -0600

    Corrected calculate method to Vue3

--- Week8/ex2/calc.js
+++ Week8/ex2/calc.js
@@ -10,7 +10,25 @@ createApp ({
         }
     },
     methods: {
-        calculate: (num1, num2) => {
+        calculate (num1, num2) {
+            switch(this.operation) {
+                case 'add':
+                    this.total = num1 + num2;
+                    break;
+                case 'subtract':
+                    this.total = num1 - num2;
+                    break;
+                case 'multiply':
+                    this.total = num1 * num2;
+                    break;
+                case 'divide':
+                    this.total = num1 / num2;
+                    break;
+
+                default:
+                    window.alert('unexpected operator');
+                    break;
+            }
 
         }
     }
