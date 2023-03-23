c2b0795666e1a71854092dc90438e28166074530
Matthew Berg <matthewkberg@gmail.com>
2023-03-23 13:03:58 -0500

    Did my npm install and run dev, changed the page title in index.html, cleared out the App.vue script and then pasted in the Camelids data for this assignment

---
+++ week9/create-component/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+camelids: [
+      {
+        commonName: "Lama",
+        binomialName: "Lama glama",
+        order: "Artiodactyla",
+        family: "Camelidae",
+        genus: "Lama",
+        species: "L. glama",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Llama_lying_down.jpg/220px-Llama_lying_down.jpg"
+        },
+      {
+        commonName: "Alpaca",
+        binomialName: "Vicugna pacos",
+        order: "Artiodactyla",
+        family: "Camelidae",
+        genus: "Vicugna",
+        species: "V. pacos",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Alpaca_%2831562329701%29.jpg/440px-Alpaca_%2831562329701%29.jpg"
+      },
+    ]
+</script>
+
+<template>
+ 
+</template>
+
+<style scoped>
+
+</style>
