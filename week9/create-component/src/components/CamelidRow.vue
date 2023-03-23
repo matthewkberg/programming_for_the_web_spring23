9e534d2bce48b43ae0ee8cd62f8fa1547e37359f
Matthew Berg <matthewkberg@gmail.com>
2023-03-23 13:39:39 -0500

    Deleted default vue components, created CamelidRow.vue and started creating the template section for this component

---
+++ week9/create-component/src/components/CamelidRow.vue
@@ -0,0 +1,11 @@
+<template>
+    <tr>
+        <td>commonName</td>
+        <td>binomialName</td>
+        <td>order</td>
+        <td>family</td>
+        <td>genus</td>
+        <td>species</td>
+        <td><img class="camelid-img" ></td>
+      </tr>
+</template>
