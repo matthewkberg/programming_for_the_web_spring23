a835d99ae441c8db6319fc221dcc6fe8b6da62e7
Matthew Berg <matthewkberg@gmail.com>
2023-03-26 14:41:48 -0500

    added css style, and began building pieces of the FilmRow component in FilmRow.vue

---
+++ week9/film-vue/src/components/FilmRow.vue
@@ -0,0 +1,41 @@
+<script setup>
+    
+</script>
+
+<template>
+    <tr>
+        <td><img class="poster-img"></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>
+            <button type="button">Delete</button>
+        </td>
+    </tr>
+</template>
+
+<style scoped>
+.poster-img {
+    max-width: 100px;
+    padding: 5px;
+}
+
+button {
+    border-style: none;
+    border-radius:50px;
+    min-width: 150px;
+    min-height: 50px;
+    font-family: 'Bungee', serif;
+    color: #ede2da;
+    background-color: #962fbf;
+}
+
+.odd {
+    background-color: #d3b8a4;
+}
+
+.even {
+    background-color: #ede2da;
+}
+</style>
