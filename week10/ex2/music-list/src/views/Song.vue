492aedb5e1203b26aa5fff56b0b7a38f11b2bb9c
Matthew Berg <matthewkberg@gmail.com>
2023-03-30 13:27:10 -0500

    imported MusicList data, setup useRoute with parametes

--- week10/ex2/music-list/src/views/Song.vue
+++ week10/ex2/music-list/src/views/Song.vue
@@ -1,3 +1,11 @@
+<script setup>
+import { useRoute } from 'vue-router';
+import MusicList from "../data/music-list.json";
+
+const route = useRoute();
+console.log(route.params.slug)
+</script>
+
 <template>
     <div>Song</div>
 </template>
