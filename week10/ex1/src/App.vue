f2602faf749a156692f88d2dcb0c8b525840d11c
Matthew Berg <matthewkberg@gmail.com>
2023-03-30 12:37:46 -0500

    Add css style to App.vue for how the container class and nav appears

--- week10/ex1/src/App.vue
+++ week10/ex1/src/App.vue
@@ -14,6 +14,49 @@ import { RouterLink, RouterView } from 'vue-router'
   </div>
 </template>
 
-<style scoped>
+<style>
+html {
+    font-size: 16px;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
 
+body, #app {
+  margin: 0;
+  padding: 0;
+}
+
+nav {
+  width: 100%;
+  height: 45px;
+  background: rgb(15, 15, 79);
+}
+
+nav a, nav a:hover, nav a:link, nav a:active, nav a:visited {
+  color: #fff;
+  font-weight: 900;
+  font-size: 24px;
+}
+
+.container {
+    max-width: 1000px;
+    width: 100%;
+    margin: 0 auto;
+}
+
+.container h1 {
+  text-align: center;
+  font-size: 48px;
+}
+
+.container p {
+  width: 800px;
+  font-size: inherit;
+}
+
+.container img {
+    display: block;
+    max-width: 600px;
+    width: 100%;
+    margin: 20px auto 20px;
+}
 </style>
