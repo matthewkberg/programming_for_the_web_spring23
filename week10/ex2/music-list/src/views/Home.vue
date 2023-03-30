5af66197711778bd23e386f29f9e46ae808c8aea
Matthew Berg <matthewkberg@gmail.com>
2023-03-30 13:20:21 -0500

    Created song list with RouterLink and linked to music-list.json to bring in song data

--- week10/ex2/music-list/src/views/Home.vue
+++ week10/ex2/music-list/src/views/Home.vue
@@ -4,5 +4,12 @@ console.log("Music list", MusicList);
 </script>
 
 <template>
-    <div>This is Home</div>
+    <div class="song-container">
+        <div class="song-list-item" v-for="(song, index) in MusicList" :key="index">
+        <RouterLink :to="'/song/' + song.slug">
+            {{ song.title }}
+        </RouterLink>
+        </div>
+    </div>
+
 </template>
