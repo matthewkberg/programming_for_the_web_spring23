0501c4932a33f4a236c6f7b4fb707c5df0055c6c
Matthew Berg <matthewkberg@gmail.com>
2023-03-30 13:40:12 -0500

    Created template html that links to MusicList data and added class styling based on current song rank in relation to last week

--- week10/ex2/music-list/src/views/Song.vue
+++ week10/ex2/music-list/src/views/Song.vue
@@ -4,8 +4,36 @@ import MusicList from "../data/music-list.json";
 
 const route = useRoute();
 console.log(route.params.slug)
+const song = MusicList.find(song => {
+    return song.slug === route.params.slug;
+})
 </script>
 
 <template>
-    <div>Song</div>
+    <div class="song-detail" :class="{
+        rising: song.rank > song.position.positionLastWeek,
+        falling: song.rank < song.position.positionLastWeek,
+        same: song.rank === song.position.positionLastWeek
+        }">
+        <h1>{{ song.rank }}</h1>
+        <h2>{{ song.title }}</h2>
+        <img :src="song.cover" :alt="song.title">
+        <p>Artist: {{ song.artist }}</p>
+        <p>Peak Position: {{ song.position.peakPosition }}</p>
+        <p>Weeks on Chart: {{ song.position.weeksOnChart }}</p>
+    </div>
 </template>
+
+<style>
+.song-detail.rising {
+    border: 1px solid green;
+}
+
+.song-detail.falling {
+    border: 1px solid red;
+}
+
+.song-detail.same {
+    border: 1px solid blue;
+}
+</style>
