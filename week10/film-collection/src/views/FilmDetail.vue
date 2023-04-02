03845dfcc5e1f92e3d49aa523b4c7fd8e0466683
Matthew Berg <matthewkberg@gmail.com>
2023-04-02 18:37:18 -0500

    Updated film-list.json so poster images were no longer web links and linked to films folder instead / Built out FilmDetail view to include posters, synopsis, and genres

--- week10/film-collection/src/views/FilmDetail.vue
+++ week10/film-collection/src/views/FilmDetail.vue
@@ -11,6 +11,20 @@ const filmDetails = filmData.find((film) => {
 
 <template>
     <div>
-        <div>{{ filmDetails.title }}</div>
+        <div>
+            <h1 class="title">{{ filmDetails.title }}</h1>
+            <img 
+            class="film-img" 
+            :src="`/films/${filmDetails.poster}`" 
+            :alt="filmDetails.title"/>
+            <div>
+                <h3>Synopsis</h3>
+                <p>{{ filmDetails.synopsis }}</p>
+            </div>
+            <div>
+                <h3>Genres</h3>
+                <p>{{ filmDetails.genres }}</p>
+            </div>
+        </div>
     </div>
 </template>
