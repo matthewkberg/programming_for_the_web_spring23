5275d23de7ae4ebace850daf72d17eca26ec793c
Matthew Berg <matthewkberg@gmail.com>
2023-04-02 17:46:48 -0500

    Add text for RouterLink to in HomeView, started setting up FilmView by importing my film-list.json file, and being to write code for bringing in card components

--- week10/film-collection/src/views/FilmView.vue
+++ week10/film-collection/src/views/FilmView.vue
@@ -1,3 +1,10 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import filmData from '../data/film-list.json'
+</script>
+
 <template>
-    <div>Films</div>
+    <div>
+        <div :key="film.id" v-for="film in filmData">{{ film.title }}</div>
+    </div>
 </template>
