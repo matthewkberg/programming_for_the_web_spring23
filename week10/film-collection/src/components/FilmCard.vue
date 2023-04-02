ad4642423ae4b9f76e42e2c70ff0de6f27c69f5e
Matthew Berg <matthewkberg@gmail.com>
2023-04-02 18:11:18 -0500

    Built out FilmCard component, imported filmCard component to FilmView

---
+++ week10/film-collection/src/components/FilmCard.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+defineProps(["id", "name", "poster"])
+</script>
+
+<template>
+    <div class="film-card">
+        <img class="film-img" :src="poster" :alt="title" />
+        <div class="footer">
+            <RouterLink class="footer-link" :to="`/films/${id}`">{{ name }}</RouterLink>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.film-card {
+    max-width: 200px;
+    border: 1px solid rgb(22, 22, 56);
+    border-radius: 5px;
+    margin-right: 10px;
+    margin-bottom: 10px;
+}
+
+.film-img {
+    max-width: 200px;
+    display: block;
+    width: 100%;
+}
+
+.footer {
+    text-align: center;
+    border-top: 1px solid rgb(22, 22, 56);
+    padding-top: 10px;
+}
+
+.footer-link {
+
+}
+</style>
--- week10/film-collection/src/views/FilmView.vue
+++ week10/film-collection/src/views/FilmView.vue
@@ -1,10 +1,16 @@
 <script setup>
 import { RouterLink } from 'vue-router';
 import filmData from '../data/film-list.json'
+import filmCard from '../components/FilmCard.vue'
 </script>
 
 <template>
     <div>
-        <div :key="film.id" v-for="film in filmData">{{ film.title }}</div>
+        <filmCard 
+        :key="film.id" 
+        v-for="film in filmData"
+        :id="film.id"
+        :name="film.title"
+        :poster="film.poster"/>
     </div>
 </template>
