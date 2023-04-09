b43c12f6c9b061cdd462218564bbfa5bba8849b9
Matthew Berg <matthewkberg@gmail.com>
2023-04-09 15:39:02 -0500

    Got my blob to appear with a random fill looping at a crazy high rate.

--- week11/iteration/iteration.js
+++ week11/iteration/iteration.js
@@ -1,4 +1,18 @@
 function setup () {
     createCanvas(1200, 800);
     background(0);
+    angleMode(DEGREES);
 }
+
+function makeBlob() {
+    noStroke();
+    fill(random(255), random(255), random(255));
+    quad(300, 600, 400, 600, 750, 800, 400, 800)
+
+}
+
+function draw() {
+    makeBlob();
+
+}
+
