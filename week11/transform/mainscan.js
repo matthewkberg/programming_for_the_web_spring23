039138576778cb5b0356656a26c9e6e2bdc75497
Matthew Berg <matthewkberg@gmail.com>
2023-04-09 17:26:38 -0500

    used copy function to set what to copy from the video input and where to copy it to, told p5 to load the video pixels

--- week11/transform/mainscan.js
+++ week11/transform/mainscan.js
@@ -9,4 +9,20 @@ function setup() {
 
 function draw () {
     background(200);
+    video.loadPixels();
+
+    var w = video.width;
+    var h = video.height;
+
+// telling what to copy and where to copy it to
+    copy(
+         video, // source
+         w/2, // where to start, center of video frame
+         0, // top of video frame
+         1, // how many pixels wide
+         h, // how many pixels tall
+         0, 0, // where to copy to on the canvas
+         1, // how many pixels wide
+         h // same height
+         )
 }
