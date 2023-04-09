9b0940c9cdbe489d8966167e289d53bff80cf330
Matthew Berg <matthewkberg@gmail.com>
2023-04-09 16:24:24 -0500

    Add a frameRate delay of 1 frame to the draw function, added text telling the use to press their mouse to pause the loop, the loop resumes once mouse is released.

--- week11/iteration/iteration.js
+++ week11/iteration/iteration.js
@@ -4,13 +4,12 @@ function setup () {
     createCanvas(1200, 800);
     for (let i = 0; i < 500; i++) {
        blob[i] = new Blob();
-        
     }
 }
 
-
 function draw() {
-    background(0);
+    background(200);
+    frameRate(1);
     for (let i = 0; i < blob.length; i++) {
         blob[i].move();
         blob[i].show();
@@ -35,5 +34,19 @@ class Blob {
         strokeWeight(3);
         noFill();
         rect(this.x, this.y, this.x, this.y);
+        textFont('Georgia');
+        textSize(24);
+        fill(0);
+        noStroke();
+        textAlign(CENTER);
+        text('Press to pause!', width / 2, height / 2);
     }
+}
+
+function mousePressed() {
+    noLoop();
+}
+
+function mouseReleased() {
+    loop();
 }
