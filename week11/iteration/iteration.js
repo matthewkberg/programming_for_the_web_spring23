9021a9bcede6cdf6784597aced96fbec577b6615
Matthew Berg <matthewkberg@gmail.com>
2023-04-09 16:00:59 -0500

    Decided to restart by rebuild my bubble assignment using a rectangle.
    Going to make adjustments from here.

--- week11/iteration/iteration.js
+++ week11/iteration/iteration.js
@@ -1,21 +1,39 @@
+let blob = [];
+
 function setup () {
     createCanvas(1200, 800);
-    background(0);
-    angleMode(DEGREES);
+    for (let i = 0; i < 500; i++) {
+       blob[i] = new Blob();
+        
+    }
 }
 
-function makeBlob() {
-    stroke(random(255), random(255), random(255));
-    strokeWeight(3);
-    noFill();
-    rect(300, 60, 400, 60);
-
-}
 
 function draw() {
-    makeBlob();
-    translate(random(50), random(75));
-    rotate(random(360));
-
+    background(0);
+    for (let i = 0; i < blob.length; i++) {
+        blob[i].move();
+        blob[i].show();
+    }
 }
 
+class Blob {
+    constructor() {
+        this.x = 200;
+        this.y = 150;
+
+    }
+
+    move() {
+        this.x = this.x + random(-5, 5);
+        this.y = this.y + random(-5, 5);
+
+    }
+
+    show() {
+        stroke(random(255), random(255), random(255));
+        strokeWeight(3);
+        noFill();
+        rect(this.x, this.y, this.x, this.y);
+    }
+}
