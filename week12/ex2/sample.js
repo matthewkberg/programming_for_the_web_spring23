a28eca62113f11ed960c35468fc93c23087c2c63
Matthew Berg <matthewkberg@gmail.com>
2023-04-13 16:05:51 -0500

    Added additional ellipses, which broke my slider volume control so I get rid of the slider and went with a set volume instead

--- week12/ex2/sample.js
+++ week12/ex2/sample.js
@@ -14,6 +14,7 @@ function setup() {
 function togglePlaying() {
     if (!song.isPlaying()) {
         song.play();
+        song.setVolume(0.25);
         button.html("pause")
     } else {
         song.pause();
@@ -25,16 +26,24 @@ function togglePlaying() {
 function loaded() {
     button = createButton("play");
     button.mousePressed(togglePlaying);
-    slider = createSlider(0, 1, 0.25, 0.01);
+
 }
 
 function draw() {
     background(77, 101, 164);
     var vol = amp.getLevel();
     var diam = map(vol, 0, 1, 10, 200);
+
+    fill(143, 177, 177);
+    noStroke();
+    ellipse(200, 100, diam*4, diam*4);
+
+    fill(253, 190, 209);
+    noStroke();
+    ellipse(400, 250, diam*6, diam*6);
     
     fill(249, 207, 149);
     noStroke();
     ellipse(width/2, height/2, diam, diam);
-    song.setVolume(slider.value());
+
 }
