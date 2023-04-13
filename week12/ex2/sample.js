41cb5e2a473094260dd5f51cc72d0e9235b707ae
Matthew Berg <matthewkberg@gmail.com>
2023-04-13 15:46:54 -0500

    moved my button and slider into the loaded function to keep them from appearing before the song was ready

--- week12/ex2/sample.js
+++ week12/ex2/sample.js
@@ -6,9 +6,7 @@ var button;
 function setup() {
     createCanvas(600, 400);
     song = loadSound("heart.mp3", loaded);
-    button = createButton("play");
-    button.mousePressed(togglePlaying);
-    slider = createSlider(0, 1, 0.25, 0.01);
+
 }
 
 function togglePlaying() {
@@ -23,7 +21,9 @@ function togglePlaying() {
 }
 
 function loaded() {
-  //  song.play();
+    button = createButton("play");
+    button.mousePressed(togglePlaying);
+    slider = createSlider(0, 1, 0.25, 0.01);
 }
 
 function draw() {
