1ef0e8c6bc6c2a5143bd3dafa58ebed58d74918d
Matthew Berg <matthewkberg@gmail.com>
2023-04-13 15:38:39 -0500

    created a play button so my song doesn't autoplay once loaded

--- week12/ex2/sample.js
+++ week12/ex2/sample.js
@@ -1,15 +1,22 @@
 var song;
 var slider;
+var button;
 
 
 function setup() {
     createCanvas(800, 600);
     song = loadSound("heart.mp3", loaded);
+    button = createButton("play");
+    button.mousePressed(togglePlaying);
     slider = createSlider(0, 1, 0.25, 0.01);
 }
 
-function loaded() {
+function togglePlaying() {
     song.play();
+}
+
+function loaded() {
+  //  song.play();
 }
 
 function draw() {
