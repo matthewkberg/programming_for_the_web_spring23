2caa1e0795cddf260e2e26f0933bdb654a8034bb
Matthew Berg <matthewkberg@gmail.com>
2023-04-20 13:37:07 -0500

    Replaced the circle with curved lines, created a second sin function with pos and inc values, used it to make a second amplified variable, then plug both amplified variables into my curve

--- week13/ex2/oscillation.js
+++ week13/ex2/oscillation.js
@@ -6,7 +6,10 @@ function setup() {
     angleMode(DEGREES);
 
     pos = 0.0; //float varible
-    inc = 3.0   //determines the frequency of the sin value 
+    inc = 3.0;   //determines the frequency of the sin value
+
+    pos2 = 0.0;
+    inc2 = 7.0;
 
 }
 
@@ -15,13 +18,17 @@ function draw() {
     translate(width/2, height/2); //begin drawing at the center of the canvas
 
     var mySinVal = sin(pos); //sin function call of the position
+    var mySinVal2 = sin(pos2);
+
     amplified = mySinVal * 50; //multiplied to increase the movement result
+    amplified2 = mySinVal2 * 50;
 
     for (x = 0; x <= 360; x = x + 10) { //loop that starts at 0 and increases by 10
-        ellipse(0, 10, 100, 100);
+        curve(0, 10, 20, amplified2, amplified, 50, 60, 70);
         rotate(x);
     }
 
-    pos = pos + inc //position equals position PLUS the increment value set
+    pos = pos + inc; //position equals position PLUS the increment value set
+    pos2 = pos2 + inc2;
 
 }
