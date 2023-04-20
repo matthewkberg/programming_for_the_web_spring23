52610aa610c063d1e3b119f4344ad724d62d9cf0
Matthew Berg <matthewkberg@gmail.com>
2023-04-20 13:17:14 -0500

    Drew a circle in the center of my canvas, began setting up the sin function to rotate the circle

--- week13/ex2/oscillation.js
+++ week13/ex2/oscillation.js
@@ -1,9 +1,22 @@
+var pos; //position
+var inc; //increment
+
 function setup() {
     createCanvas(800, 600);
+    angleMode(DEGREES);
+
+    pos = 0.0; //float varible
+    inc = 3.0   //determines the frequency of the sin value 
 
 }
 
 function draw() {
     background(175);
-    
+
+    var mySinVal = sin(pos); //sin function call of the position
+
+    ellipse(width/2, height/2, 100, 100);
+
+    pos = pos + inc //position equals position PLUS the increment value set
+
 }
