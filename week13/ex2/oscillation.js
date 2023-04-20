81dbe853f1d1a3894b3f1250a897c6d47ccec0e5
Matthew Berg <matthewkberg@gmail.com>
2023-04-20 13:27:32 -0500

    created a for loop to rotate an ellipse, set the drawing to begin at the center of the canvas.

--- week13/ex2/oscillation.js
+++ week13/ex2/oscillation.js
@@ -12,12 +12,15 @@ function setup() {
 
 function draw() {
     background(175);
+    translate(width/2, height/2); //begin drawing at the center of the canvas
 
     var mySinVal = sin(pos); //sin function call of the position
+    amplified = mySinVal * 50; //multiplied to increase the movement result
 
-    amplified = mySinVal * 50;
-
-    ellipse(amplified + 150, height/2, 100, 100);
+    for (x = 0; x <= 360; x = x + 10) { //loop that starts at 0 and increases by 10
+        ellipse(0, 10, 100, 100);
+        rotate(x);
+    }
 
     pos = pos + inc //position equals position PLUS the increment value set
 
