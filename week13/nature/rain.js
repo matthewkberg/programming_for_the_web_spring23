61d2345f9e94d219f126f09703ff2079fec647a1
Matthew Berg <matthewkberg@gmail.com>
2023-04-24 12:44:19 -0500

    created initial function for drawing a single raindrop, set up drop variable

--- week13/nature/rain.js
+++ week13/nature/rain.js
@@ -1,9 +1,24 @@
+var drop;
+
 function setup() {
     createCanvas(800, 600);
+    drop = new Drop();
 
 }
 
 function draw() {
     background('#0a2136');
+    drop.show();
     
+}
+
+function Drop() {
+    this.x = width/2;
+    this.y = 30;
+
+    this.show = function() {
+        noStroke();
+        fill(138, 43, 226);
+        ellipse(this.x, this.y, 2, 10);
+    }
 }
