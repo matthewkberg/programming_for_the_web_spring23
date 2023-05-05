27b08713e88a5dab124317755e0e2d85b3c0297e
Matthew Berg <matthewkberg@gmail.com>
2023-05-05 13:08:48 -0500

    Added a second waveform, in front of my Quad Strips, and gave it a higher y position for it's first vertex!

--- week15/final/sketch.js
+++ week15/final/sketch.js
@@ -94,6 +94,17 @@ function draw() {
         volhistory.splice(0, 1);
     }
 
+    noStroke();
+    fill(colors[i % colors.length]);
+    beginShape();
+    for (let i = 0; i < waveform.length; i++){
+        let x = map(i, 0, waveform.length, 0, width);
+        let y = map( waveform[i], -1, 1, 0, height);
+        vertex(x,y - 150);
+        vertex(x,y - 5);
+        }
+    endShape();
+
     for (var i = 0; i < 500; i++) {
         tree[i].show();
     };
